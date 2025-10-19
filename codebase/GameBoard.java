192f33f21aba8bcbfdbbe67b083df0c3c2e84bb1
Sudhir <sudhboiii@gmail.com>
2025-10-19 10:59:43 -0300

    Added GameBoard Class

---
+++ codebase/GameBoard.java
@@ -0,0 +1,9 @@
+public class GameBoard {
+    private int[][] boardState;
+    private int[][] boardColors;
+
+    public GameBoard (int[][] boardState, int[][] boardColors) {
+        this.boardState = boardState;
+        this.boardColors = boardColors;
+    }
+}
